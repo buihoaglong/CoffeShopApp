04c9bff4c8500c6e714234c0bcff73a2a608065f
buihoaglong <MontestBHL@gmail.com>
2025-03-08 08:30:54 +0700

    update
    update splash activity

---
+++ coffeShopApp/app/src/main/java/com/example/coffeshopapp/Activity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.coffeshopapp.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeshopapp.R
+import com.example.coffeshopapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+
+        }
+    }
+}
